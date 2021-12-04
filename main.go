c2bdac27f1ce5f05dd0a5d67ac2e33fc2fb50da4
Bart6114 <bart@dataroots.io>
2021-12-04 22:26:50 +0100

    progress

--- main.go
+++ main.go
@@ -1,35 +1,146 @@
 package main
 
 import (
-	"errors"
+	"bufio"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"os/exec"
+	"strings"
 	"time"
 
 	"gopkg.in/yaml.v3"
 
 	"github.com/adhocore/gronx"
-	"github.com/davecgh/go-spew/spew"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
-type JobsSpec struct {
-	Jobs []JobSpec `yaml:"jobs"`
+type Schedule struct {
+	Jobs map[string]*JobSpec `yaml:"jobs"`
 }
 
 type JobSpec struct {
-	Name    string `yaml:"name"`
-	Cron    string `yaml:"cron"`
-	Command string `yaml:"command"`
-	startAt time.Time
+	Cron           string   `yaml:"cron"`
+	Command        string   `yaml:"command"`
+	Triggers       []string `yaml:"triggers"`
+	name           string
+	globalSchedule *Schedule
+	runs           []time.Time
+	statuses       []int
+	logTails       []string
 }
 
-func (j *JobSpec) Do() {
+func readSpecs(fn string) (Schedule, error) {
+	yfile, err := ioutil.ReadFile(fn)
+
+	if err != nil {
+		log.Error().Err(err)
+		return Schedule{}, err
+	}
+
+	specs := Schedule{}
+
+	if err = yaml.Unmarshal(yfile, &specs); err != nil {
+
+		log.Error().Err(err)
+		return Schedule{}, err
+	}
+
+	return specs, nil
+
+}
+
+func LoadSchedule(fn string) (Schedule, error) {
+	s, err := readSpecs(fn)
+	if err != nil {
+		return Schedule{}, err
+	}
+
+	// run validations
+
+	for k, v := range s.Jobs {
+		// validate cron string
+		if v.Cron != "" {
+			gronx := gronx.New()
+			if !gronx.IsValid(v.Cron) {
+				return Schedule{}, fmt.Errorf("cron string for job '%s' not valid", k)
+
+			}
+		}
+		// check if trigger references exist
+		for _, t := range v.Triggers {
+			if _, ok := s.Jobs[t]; !ok {
+				return Schedule{}, fmt.Errorf("cannot find spec of job '%s' that is referenced in job '%s'", t, k)
+			}
+
+		}
+		// set name for easy access
+		v.name = k
+		v.globalSchedule = &s
+		// s.Jobs[k] = v
+	}
+
+	return s, nil
+}
+
+func (j *JobSpec) ExecCommand(trigger string) {
+	log.Info().Str("Job", j.name).Str("Trigger", trigger).Msgf("Job triggered by.")
+	// register new run
+	j.runs = append(j.runs, time.Now())
+	j.logTails = append(j.logTails, "")
+
+	cmdString := parseCommand(j.Command)
+	var args []string
+	if len(cmdString) > 1 {
+		args = cmdString[1:]
+	}
+	cmd := exec.Command(cmdString[0], args...)
+
+	pipe, _ := cmd.StdoutPipe()
+	if err := cmd.Start(); err != nil {
+		// handle error
+	}
+	reader := bufio.NewReader(pipe)
+	line, err := reader.ReadString('\n')
+	for err == nil {
+		// output to stdout
+		fmt.Print(line)
+		// output to our logger
+		j.logTails[len(j.logTails)-1] = j.logTails[len(j.logTails)-1] + line + "\n"
+		line, err = reader.ReadString('\n')
+	}
+
+	if err := cmd.Wait(); err != nil {
+		if exitError, ok := err.(*exec.ExitError); ok {
+			j.statuses = append(j.statuses, exitError.ExitCode())
+		}
+		return
+	}
+
+	j.statuses = append(j.statuses, 0)
+	// trigger jobs that should run on succesful completion
+	for _, tn := range j.Triggers {
+		tj := j.globalSchedule.Jobs[tn]
+		go func() {
+			tj.ExecCommand(tn)
+		}()
+
+	}
+
+}
+
+func (j *JobSpec) Schedule() {
+
+	if j.Cron == "" {
+		log.Info().Str("Job", j.name).Msg("No cron string specified, will wait to be actively triggered.")
+		return
+	}
+
 	gronx := gronx.New()
 
-	if gronx.IsValid(j.Cron) != true {
-		log.Fatal().Msg("cron string not valid")
+	if !gronx.IsValid(j.Cron) {
+		log.Fatal().Str("Job", j.name).Msg("Cron string not valid.")
 	}
 
 	ticker := time.NewTicker(time.Second)
@@ -39,66 +150,42 @@ func (j *JobSpec) Do() {
 			due, _ := gronx.IsDue(j.Cron)
 
 			if due {
-				out, err := exec.Command(j.Command).Output()
-				spew.Dump(out, err)
+				j.ExecCommand("cron")
 			}
-
 		}
 	}()
 }
 
-func getGron(cronExpr string) (gronx.Gronx, error) {
-	gron := gronx.New()
-	if !gron.IsValid(cronExpr) {
-		return gronx.Gronx{}, errors.New("Invalid CRON expression")
-	}
-
-	return gron, nil
-
+func parseCommand(command string) []string {
+	return strings.Fields(command)
 }
 
-func readSpecs(fn string) (JobsSpec, error) {
-	yfile, err := ioutil.ReadFile(fn)
+func initSchedule(scheduleFn string) {
 
+	js, err := LoadSchedule(scheduleFn)
 	if err != nil {
-		log.Error().Err(err)
-		return JobsSpec{}, err
+		log.Error().Err(err).Msg("")
+		os.Exit(1)
+	}
+	numberJobs := len(js.Jobs)
+	i := 0
+	for _, job := range js.Jobs {
+		log.Info().Msgf("Initializing (%v/%v) job: %s", i, numberJobs, job.name)
+		job.Schedule()
+		i = i + 1
 	}
 
-	specs := JobsSpec{}
-
-	err2 := yaml.Unmarshal(yfile, &specs)
-
-	if err2 != nil {
-
-		log.Error().Err(err)
-		return JobsSpec{}, err
+	for {
 	}
 
-	return specs, nil
-
-}
-
-func bgTask() {
-	ticker := time.NewTicker(1 * time.Second)
-	for _ = range ticker.C {
-		fmt.Println("Tock")
-	}
 }
 
 func main() {
-	fmt.Println("Go Tickers Tutorial")
-
-	go bgTask()
-
-	// This print statement will be executed before
-	// the first `tock` prints in the console
-	fmt.Println("The rest of my application can continue")
-
-	// here we use an empty select{} in order to keep
-	// our main function alive indefinitely as it would
-	// complete before our backgroundTask has a chance
-	// to execute if we didn't.
-	select {}
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) != 1 {
+		panic("Please pass a schedule file as first argument.")
+	}
+	initSchedule(argsWithoutProg[0])
 
 }
