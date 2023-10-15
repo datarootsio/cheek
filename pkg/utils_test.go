435be3d434aa3fb3e1506d6f950dae9fdb79d1ff
suitap <suitap@users.noreply.github.com>
2023-10-15 11:46:45 +0900

    use assert package

--- pkg/utils_test.go
+++ pkg/utils_test.go
@@ -11,39 +11,21 @@ import (
 
 func TestLastLineReader(t *testing.T) {
 	l, err := readLastLines("../testdata/test.jsonl", 2)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(l) != 2 {
-		t.Fatal("incorrect number of lines")
-	}
-	if l[0] != "{\"a\":4}\n" {
-		t.Fatal("incorrect line in first place")
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(l), "incorrect number of lines")
+	assert.Equal(t, "{\"a\":4}\n", l[0], "incorrect line in first place")
 
 	// go over number of lines in file
 	l, err = readLastLines("../testdata/test.jsonl", 20)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(l) != 4 {
-		t.Fatal("incorrect number of lines")
-	}
-	if l[0] != "{\"a\":4}\n" {
-		t.Fatal("incorrect line in first place")
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(l), "incorrect number of lines")
+	assert.Equal(t, "{\"a\":4}\n", l[0], "incorrect line in first place")
 
 	// read everything
 	l, err = readLastLines("../testdata/test.jsonl", -1)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(l) != 4 {
-		t.Fatal("incorrect number of lines")
-	}
-	if l[0] != "{\"a\":4}\n" {
-		t.Fatal("incorrect line in first place")
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, 4, len(l), "incorrect number of lines")
+	assert.Equal(t, "{\"a\":4}\n", l[0], "incorrect line in first place")
 }
 
 func TestHardWrap(t *testing.T) {
